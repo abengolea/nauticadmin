7ef2927a528006175c03da82fe1d183ab4d187cc
Adrian Bengolea <abengolea1@gmail.com>
2026-02-07 17:20:11 +0000

    usar el logo que te pase. no crre un logo - usa el logo de jpeg

--- src/components/icons/RiverPlateLogo.tsx
+++ src/components/icons/RiverPlateLogo.tsx
@@ -1,22 +1,14 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function RiverPlateLogo({ className }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 120"
-      className={cn("h-8 w-8", className)}
-      fill="none"
-    >
-      <path
-        d="M50 0L95 25V90L50 115L5 90V25L50 0Z"
-        className="fill-card stroke-border"
-        strokeWidth="3"
-      />
-      <path
-        d="M33 35L95 25L67 80L5 90L33 35Z"
-        className="fill-primary"
-      />
-    </svg>
+    <Image
+      src="/river-plate-logo.png"
+      alt="Escuela de River Plate Logo"
+      width={40}
+      height={40}
+      className={cn("", className)}
+    />
   );
 }
